d67b9717abc7f6ba8827c49c01e81e4da624bfaa
QualityPre <qualitypre@protonmail.com>
2022-03-09 11:07:28 +0000

    footer and routes done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+//pages
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
 
 //components
 import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 const App = () => {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main>Main Content here</main>
+        <main className="container mx-auto px-4 pb-10">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   )
---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import { FaHome } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <p className="text-4xl"> Whoops!</p>
+    </div>
+  )
+}
+
+export default NotFound
